6b9911b953160ecb9622e8bbec8740552c9177b4
dszwarc <dszwarc93@gmail.com>
2023-12-18 16:13:06 -0500

    added listing model and set up database with seed

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,33 +1,11 @@
 <?php
-    namespace App\Models;
 
+namespace App\Models;
 
-    //Create class for Listing object
-    class Listing {
-        //when running all function, return all listings as object. in this example we have the listings hard-coded
-        public static function all(){
-            return [
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, nesciunt?'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, nesciunt?'
-                ]
-                ];
-        }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-        // use id from GET Route in order to find a specific listing with that id
-        public static function find($id) {
-            $listings = self::all();
-
-            foreach($listings as $listing){
-                if($listing['id']==$id){
-                    return $listing;
-                }
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
+}
